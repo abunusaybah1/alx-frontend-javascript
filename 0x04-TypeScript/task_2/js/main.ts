e83c56b7ecec2393f51b14d2d36f0e1ae48e0e53
abunusaybah <your_github_email@example.com>
2025-11-08 20:09:24 +0100

    Completed task 7

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,79 @@
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome(): string {
+		return "Working from home";
+	}
+
+	getCoffeeBreak(): string {
+		return " Getting a coffee break";
+	}
+
+	workDirectorTasks(): string {
+		return "Getting to director tasks";
+	}
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return "Cannot work from home";
+	}
+
+	getCoffeeBreak(): string {
+		return "Cannot have a break ";
+	}
+
+	workTeacherTasks(): string {
+		return "Getting to work";
+	}
+}
+
+function createEmployee(salary: string | number) {
+	if (typeof salary === "number") {
+		if (salary < 500) {
+			return new Teacher();
+		}
+	} else {
+		return new Director();
+	}
+}
+
+// console.log(createEmployee(200));
+
+console.log(createEmployee(1000));
+
+export function isDirector(employee: Director | Teacher): employee is Director {
+	return (employee as Director).workDirectorTasks !== undefined;
+	// the above line checks if the method workDirectorTasks exists on the employee object and narrows the type accordingly
+}
+
+function executeWork(employee: Director | Teacher) {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	} else {
+		return employee.workTeacherTasks();
+	}
+}
+
+type Subjects = "Maths" | "History";
+
+function teachClass(todayClass: Subjects) {
+	if (todayClass === "Maths") {
+		return "Teaching Maths";
+	} else {
+		return "Teaching History";
+	}
+}
+
+console.log(teachClass("Maths"));
+console.log(teachClass("History"));
